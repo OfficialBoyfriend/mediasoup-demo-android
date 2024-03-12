3c530c563eb78444793245415dcd94cc88c9b582
zeronumber <admin@oleander.one>
2024-03-12 22:34:23 +0800

    chore: 整理代码

--- app/src/main/java/org/mediasoup/droid/demo/Application.java
+++ app/src/main/java/org/mediasoup/droid/demo/Application.java
@@ -4,13 +4,13 @@ import org.mediasoup.droid.Logger;
 import org.mediasoup.droid.MediasoupClient;
 
 public class Application extends android.app.Application {
-
   @Override
   public void onCreate() {
     super.onCreate();
 
     Logger.setLogLevel(Logger.LogLevel.LOG_DEBUG);
     Logger.setDefaultHandler();
+
     MediasoupClient.initialize(getApplicationContext());
   }
 }
